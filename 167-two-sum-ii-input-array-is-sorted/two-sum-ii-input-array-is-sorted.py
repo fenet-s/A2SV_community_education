55e8ee3b6f11653fdd76edf99b6c8be821a5c79c
fenet-s <fenetseifudin1@gmail.com>
2025-10-18 09:58:32 +0300

    Time: 3 ms (81.15%) | Memory: 18.5 MB (59.37%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left,right=0,len(numbers)-1
+        ans=[]
+        while left < right:
+            if numbers[left]+numbers[right]==target:
+                ans.extend([left+1,right+1])
+                break
+            elif numbers[left]+numbers[right]< target:
+                left+=1
+            else:
+                right-=1
+        return ans
+        
